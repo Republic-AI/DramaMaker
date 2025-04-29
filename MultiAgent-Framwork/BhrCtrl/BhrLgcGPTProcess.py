029dc56adbd7cc060e70f9dfd7322da45fc9e38f
zombie <1715546494@qq.com>
2025-04-29 11:52:16 +0800

    deepseek

--- MultiAgent-Framwork/BhrCtrl/BhrLgcGPTProcess.py
+++ MultiAgent-Framwork/BhrCtrl/BhrLgcGPTProcess.py
@@ -37,12 +37,12 @@ if is_chatgpt:
     model_large = "gpt-4o"
 else:
     # client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=deepseek_key) 
-    client = OpenAI(base_url="https://api.gmi-serving.com/v1", api_key=deepseek_key) 
+    client = OpenAI(base_url="https://api.deepseek.com", api_key=deepseek_key) 
     client_embedding = OpenAI(api_key=openai_key)
     # model_small = "deepseek/deepseek-r1-distill-llama-70b"
     # model_large = "deepseek/deepseek-r1-distill-llama-70b"
-    model_small = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
-    model_large = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
+    model_small = "deepseek-chat"
+    model_large = "deepseek-chat"
 
 
 yaml_path = os.path.join(base_dir, 'char_config.yaml')
